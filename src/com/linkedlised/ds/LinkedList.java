4167b361418d7a3ad3a672df5d796b6f54b5883e
sayrish <sayrrish1@gmail.com>
2022-10-31 14:22:10 +0530

    Add Element Position

--- src/com/linkedlised/ds/LinkedList.java
+++ src/com/linkedlised/ds/LinkedList.java
@@ -17,6 +17,41 @@ public class LinkedList {
 			newNode.next = head;
 			head = newNode;
 		}
+	public void addLast(Object data)
+	{
+		Node newNode = new Node(data);
+		Node temp = head;
+		if(head == null) {
+			head = newNode;
+		}
+			
+		else
+		{
+			while(temp.next != null)
+			{
+				temp = temp.next;
+			}
+			temp.next = newNode;
+		}
+	}
+	public void addPosition(Object data, int position)
+	{
+		int index =0;
+		Node newNode = new Node(data);
+		Node left = head;
+		Node right = left.next;
+		while(index < (position-1))
+		{
+			left = left.next;
+			right = right.next;
+			index++;
+			
+		}
+		newNode.next = right;
+		left.next = newNode;
+		
+		
+	}
 	public void display()
 	{
 		Node temp = head;
@@ -34,12 +69,17 @@ public class LinkedList {
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		System.out.println("Welcome to Linked List Problem");
-		list.addFirst(80);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Adding Nodes At First");
+		list.addFirst(56);
 		list.display();
-		list.addFirst(70);
+		list.addLast(70);
 		list.display();
-		list.addFirst(60);
+		System.out.println("Enter Position Number To Add:- ");
+		int position =sc.nextInt();
+		list.addPosition(30, position);
 		list.display();
+		
 	}
 
 
