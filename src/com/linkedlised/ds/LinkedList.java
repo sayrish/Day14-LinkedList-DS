f8c090dec70a6381c4523145183bdd58ac94832a
sayrish <sayrrish1@gmail.com>
2022-10-31 14:17:10 +0530

    Add Node At First

--- src/com/linkedlised/ds/LinkedList.java
+++ src/com/linkedlised/ds/LinkedList.java
@@ -2,30 +2,46 @@ package com.linkedlised.ds;
 
 public class LinkedList {
 	Node head;
-	class Node {
-		
+	class Node
+	{
 		Object data;
 		Node next;
-
-		Node(Object data) 
-		{	
+		Node(Object data )
+		{
 			this.data = data;
 		}
 	}
-
-	public void CreatList(Object data) {
-		Node newNode = new Node(data);
-		System.out.print(newNode.data + " ");
-
+	public void addFirst(Object data)
+		{
+			Node newNode = new Node(data);
+			newNode.next = head;
+			head = newNode;
+		}
+	public void display()
+	{
+		Node temp = head;
+		while(temp!=null)
+		{
+			if(temp.next != null)
+				System.out.print(temp.data+" > ");
+			else
+				System.out.println(temp.data);
+			temp = temp.next;
+		}
 	}
+	
 
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
 		System.out.println("Welcome to Linked List Problem");
-		list.CreatList(80);
-		list.CreatList(90);
-		list.CreatList(100);
+		list.addFirst(80);
+		list.display();
+		list.addFirst(70);
+		list.display();
+		list.addFirst(60);
+		list.display();
 	}
+
 
 	
 }
